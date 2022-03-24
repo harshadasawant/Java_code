990fc3cace5edf7191dc67c554caa9f033cfe9d2
Harshada sawant <harshadasawant22@gmail.com>
2022-03-24 16:22:40 +0530

    CRNB-6602 adding sout

--- src/com/company/A.java
+++ src/com/company/A.java
@@ -6,6 +6,7 @@ public class A extends AccessSpecifierExample {
     public static void main(String[] args) {
         AccessSpecifierExample obj = new AccessSpecifierExample();
        A object = new A();
+        System.out.println(object);
        object.i=9;
     }
 }
